85c38cf1bfa442d01cc146dcf0f9ac56e59d9501
Carlo Bernaschina <b3rn4@hotmail.it>
2018-03-22 11:51:39 +0100

    Fix license header

--- client/js/ifml2pcn/mobile/semantics/17-XOR-NonDefault-Child.js
+++ client/js/ifml2pcn/mobile/semantics/17-XOR-NonDefault-Child.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2018, the IFMLEdit.org project authors. Please see the
 // AUTHORS file for details. All rights reserved. Use of this source code is
-// governed by a MIT-style license that can be found in the LICENSE file.
+// governed by the MIT license that can be found in the LICENSE file.
 /*jslint node: true, nomen: true */
 "use strict";
 
