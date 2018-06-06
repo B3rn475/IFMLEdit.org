9a7ceac3ddeff159a03503427c65bed8be46a8fb
Carlo Bernaschina <b3rn4@hotmail.it>
2018-06-06 14:45:49 +0200

    Fix SystemEvent implementation

--- client/js/ifml2code/client/elementrules/viewcomponent.js
+++ client/js/ifml2code/client/elementrules/viewcomponent.js
@@ -75,7 +75,7 @@ exports.rules = [
                     .filter(function (id) { return model.isEvent(id); })
                     .filter(function (id) { return model.getOutbounds(id).length; })
                     .map(function (id) { return model.toElement(id); })
-                    .map(function (event) { return { id: model.toId(event), name: event.attributes.name}; })
+                    .map(function (event) { return { id: model.toId(event), name: event.attributes.name, stereotype: event.attributes.stereotype}; })
                     .value(),
                 obj = {
                     controls: {children: 'C-' + id}
@@ -104,7 +104,7 @@ exports.rules = [
                     .filter(function (id) { return model.isEvent(id); })
                     .filter(function (id) { return model.getOutbounds(id).length; })
                     .map(function (id) { return model.toElement(id); })
-                    .map(function (event) { return { id: model.toId(event), name: event.attributes.name}; })
+                    .map(function (event) { return { id: model.toId(event), name: event.attributes.name, stereotype: event.attributes.stereotype}; })
                     .value(),
                 obj = {
                     controls: {children: 'C-' + id}
