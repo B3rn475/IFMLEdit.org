1d364f1f743c47cdc4a4d546919913977b9ffa22
Carlo Bernaschina <b3rn4@hotmail.it>
2018-06-06 18:13:15 +0200

    Add missing file

---
+++ client/js/isvalidparent.js
@@ -0,0 +1,26 @@
+// Copyright (c) 2016, the IFMLEdit.org project authors. Please see the
+// AUTHORS file for details. All rights reserved. Use of this source code is
+// governed by the MIT license that can be found in the LICENSE file.
+/*jslint node: true */
+"use strict";
+
+var utils = require('almost-joint').utils;
+
+function isValidParent(element, parent) {
+    if (!parent) {
+        return !element.requireEmbedding;
+    }
+    if (element.fixedParent && element.get('parent')) {
+        return false;
+    }
+    var containers = element.containers || [];
+    if (-1 === containers.indexOf(parent.get('type'))) {
+        return false;
+    }
+    if (element.fullyContained) {
+        return utils.isFullyContained(element, parent);
+    }
+    return true;
+}
+
+exports.isValidParent = isValidParent;
